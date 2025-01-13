9c1b5d1d764532c4256da91620fffe0d33969d99
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2025-01-13 01:32:20 -0400

    Fix logging to safely retrieve method parameter types

--- proc_macros/src/impl_arguments_parameters.rs
+++ proc_macros/src/impl_arguments_parameters.rs
@@ -24,7 +24,7 @@ pub fn expand(range: Range<usize>) -> Result<TokenStream> {
         let log_parameters = generic_params_parameter
             .clone()
             .enumerate()
-            .map(|(n, gp)| quote!(unsafe {crate::debug!("\tChecking parameter {} {:?} vs method param {:?}", #n, stringify!(#gp), params.get_unchecked(#n).ty()); }));
+            .map(|(n, gp)| quote!(unsafe {crate::debug!("\tChecking parameter {} {:?} vs method param {:?}", #n, stringify!(#gp), params.get(#n).map(|p| p.ty())); }));
 
         let generic_params_argument_tuple = generic_params_argument.clone();
         let generic_params_argument_where = generic_params_argument.clone();
